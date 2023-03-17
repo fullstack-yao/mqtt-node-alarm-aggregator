de245bee276dc4211a2bde033d9cd7f781b318bb
Aaron <aaron@Aarons-Air.lan>
2023-03-17 14:15:32 -0500

    remove hardcode

--- alarmController.js
+++ alarmController.js
@@ -1,27 +1,27 @@
-import { FAILED_MESSAGE, NORMAL_MESSAGE } from './config.js';
-import { childTopics } from './topics.js';
+import { FAILED_MESSAGE, NORMAL_MESSAGE } from "./config.js";
+import { childTopics } from "./topics.js";
 
 export const initChildStatus = (childStatus) => {
   childTopics.forEach((childTopic) => {
     childStatus[childTopic] = NORMAL_MESSAGE;
   });
-  childStatus['numOfNormal'] = 6;
+  childStatus["numOfNormal"] = childTopics.length;
 };
 
 export const requirePubToParentTopic = (topic, messageStr, childStatus) => {
   if (messageStr === FAILED_MESSAGE) {
     if (childStatus[topic] === NORMAL_MESSAGE) {
-      childStatus['numOfNormal'] -= 1;
+      childStatus["numOfNormal"] -= 1;
     }
     childStatus[topic] = messageStr;
     return true;
   } else {
     // childStatus[topic] can be undefined or failedMessage
     if (childStatus[topic] !== messageStr) {
-      childStatus['numOfNormal'] += 1;
+      childStatus["numOfNormal"] += 1;
     }
     childStatus[topic] = messageStr;
-    if (childStatus['numOfNormal'] === childTopics.length) {
+    if (childStatus["numOfNormal"] === childTopics.length) {
       return true;
     }
   }
